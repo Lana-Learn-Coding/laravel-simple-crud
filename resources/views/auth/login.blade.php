a0ef9c173523ee05c4a0810ef99fd79091b0aa94
Lana <sitdownrightnow2552@gmail.com>
2020-03-23 23:22:50 +0700

    materialize login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,80 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
+    <v-row class="justify-center">
+        <v-col xl="3" lg="4" md="5" sm="7" cols="10">
+            <v-card>
+                <v-card-title>{{ __('login') }}</v-card-title>
+                <v-card-text>
+                    <v-form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <v-text-field
+                            type="email"
+                            label="{{  __('E-Mail Address') }}"
+                            @error('email') error @enderror
+                            error-messages="{{ $message ?? '' }}"
+                            autocomplete="email"
+                            autofocus
+                            name="email"
+                            required
+                        >
+                        </v-text-field>
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
+                        <v-text-field
+                            type="password"
+                            label="{{  __('Password') }}"
+                            @error('password') error @enderror
+                            error-messages="{{ $message ?? '' }}"
+                            name="password"
+                            autocomplete="current-password"
+                            required
+                        >
+                        </v-text-field>
+                        <v-checkbox
+                            name="remember"
+                            label="{{ __('Remember Me') }}"
+                            value="true"
+                        >
+                        </v-checkbox>
 
-                            <div class="form-group row">
-                                <label for="email"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="text-center">
+                            <v-btn type="submit" class="mr-3">
+                                {{ __('Login') }}
+                            </v-btn>
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email"
-                                           class="form-control @error('email') is-invalid @enderror" name="email"
-                                           value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="password"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
-                                           class="form-control @error('password') is-invalid @enderror" name="password"
-                                           required autocomplete="current-password">
-
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember"
-                                               id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
-                                    </button>
-
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                            @if (Route::has('password.request'))
+                                <a href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                            @endif
+                        </div>
+                    </v-form>
+                </v-card-text>
+            </v-card>
+        </v-col>
+    </v-row>
 @endsection
